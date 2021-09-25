f4b027e75391792dfe3d8377e0025b25ad246765
Ataches <jussanchezm@correo.udistrital.edu.co>
2021-09-25 07:10:47 -0500

    fix: delete old features

--- features/jugador.feature
+++
@@ -1,7 +0,0 @@
-Feature: jugador juego 21
-Esta Feature verifica las funcionalidades del jugador en el juego 21
-
-Scenario: verificar que al jugador le repartan dos cartas. 
-Given un jugador
-When el juego inicia
-Then el jugador tiene dos cartas
--- src/main/java/com/ing_software/veintiuno/Launcher.java
+++ src/main/java/com/ing_software/veintiuno/Launcher.java
@@ -2,6 +2,8 @@ package com.ing_software.veintiuno;
 
 public class Launcher { // Clase principal que ejecuta la clase veintiuno
 
+    // Based on: https://github.com/mrtesteloper/page_object_model_example
+    // Based on: https://mrtesteloper.com/2020/11/07/pruebas-bdd-con-selenium-java-y-cucumber/
     public static void main(String[] args) {
         Veintiuno veintiuno = new Veintiuno();
         veintiuno.launch();
