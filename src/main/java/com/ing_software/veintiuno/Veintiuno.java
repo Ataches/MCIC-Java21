980f126d648d9d13c9eb8e2b3b86085bea8e7b99
Ataches <jussanchezm@correo.udistrital.edu.co>
2021-09-28 07:41:12 -0500

    fix: response in methods

--- src/main/java/com/ing_software/veintiuno/Veintiuno.java
+++ src/main/java/com/ing_software/veintiuno/Veintiuno.java
@@ -6,40 +6,42 @@ public class Veintiuno { // Clase principal que dirige el juego dependiendo de l
     public static final String[] pintas = {"Corazones", "Picas", "Diamante", "Trébol"};
     public static final String[] valores = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    private final List<Carta> mazo = new ArrayList<>();
+    private List<Carta> mazo = new ArrayList<>();
     private final List<Jugador> jugadores = new ArrayList<>();
     private final Scanner sc = new Scanner(System.in);
-    private boolean jugadorContinua = true;
+    private boolean juegoContinua = true;
     private boolean finalizo = false;
 
     private final Random random = new Random();
 
 
-    public void empezarJuego() { // Metodo principal que dependiendo de la continuación o juego del jugador direcciona la partida
-        generarMazo();
+    public String empezarJuego() { // Metodo principal que dependiendo de la continuación o juego del jugador direcciona la partida
+        mazo = generarMazo();
         do {
-            jugar("jugador");
-            if (jugadorContinua) {
-                jugar("casa");
-                imprimirCartas();
-                confirmar();
+            juegoContinua = jugar("jugador");
+            if (juegoContinua) {
+                juegoContinua = jugar("casa");
+                System.out.println(imprimirCartas());
+                juegoContinua = confirmar();
             }
-        } while (jugadorContinua);
+        } while (juegoContinua);
         finalizo = true;
-        imprimirCartas();
-        System.out.println(imprimirResultado());
+        System.out.println(imprimirCartas());
+        return imprimirResultado();
     }
 
-    public void generarMazo() { // Genera el mazo a partir de las cartas que se contiene en el array y pintas
+    private List<Carta> generarMazo() { // Genera el mazo a partir de las cartas que se contiene en el array y pintas
+        List<Carta> mazo = new ArrayList<>();
         for (String pinta : pintas) {
             for (String valor : valores) {
                 Carta carta = new Carta(valor, pinta);
                 mazo.add(carta);
             }
         }
+        return mazo;
     }
 
-    public void jugar(String nombreJugador) {
+    public boolean jugar(String nombreJugador) {
         Jugador jugador = getJugador(nombreJugador);
         if (!mazo.isEmpty()) {
             if (jugador.getCartas().isEmpty()) {//Inicia mazo
@@ -48,31 +50,31 @@ public class Veintiuno { // Clase principal que dirige el juego dependiendo de l
             } else {
                 jugador.addCarta(sacarCartaMazo());
             }
-            if (!(jugador.sumarPuntos() <= 21)) //Si alguno de los jugadores paso los 21 termina el juego
-                jugadorContinua = false;
+            return jugador.sumarPuntos() <= 21; //Si alguno de los jugadores paso los 21 termina el juego
         }else {
             System.out.println("No quedan más cartas en el mazo");
-            jugadorContinua = false;
+            return false;
         }
     }
 
-    public void imprimirCartas() { //Impresiones por pantalla
+    public String imprimirCartas() { //Impresiones por pantalla
+        String mensaje = "";
         if (!finalizo) {
-            System.out.println("\nCartas casa: \n " + getJugador("casa").imprimirCartas(true));
+            mensaje += "\nCartas casa: \n " + getJugador("casa").imprimirCartas(true);
         } else {
-            System.out.println("\n*** Termino el juego ***");
-            System.out.println("\nCartas casa: \n" + getJugador("casa").imprimirCartas(false));
-            System.out.println("\nPuntaje casa: " + getJugador("casa").getPuntaje());
+            mensaje += "\n*** Termino el juego ***";
+            mensaje += "\nCartas casa: \n" + getJugador("casa").imprimirCartas(false);
+            mensaje += "\nPuntaje casa: " + getJugador("casa").getPuntaje();
         }
-        System.out.println("\nCartas jugador: \n" + getJugador("jugador").imprimirCartas(false));
-        System.out.println("Puntaje jugador: " + getJugador("jugador").getPuntaje());
+        mensaje += "\n\nCartas jugador: \n" + getJugador("jugador").imprimirCartas(false);
+        mensaje += "\nPuntaje jugador: " + getJugador("jugador").getPuntaje();
+        return mensaje;
     }
 
-    public void confirmar() { //Confirma si quiere mas cartas, sino solo juega la casa
-        System.out.println("\nJugador, quiere mas cartas? (Escriba 'No' para finalizar, escriba 'Si' para continuar): ");
+    private boolean confirmar() { //Confirma si quiere mas cartas, sino solo juega la casa
+        System.out.println("\nJugador, quiere mas cartas? (Escriba 'No' para plantar su juego): ");
         String str = sc.nextLine();
-        jugadorContinua = !"no".equalsIgnoreCase(str);
-
+        return !"no".equalsIgnoreCase(str);
     }
 
     public String imprimirResultado() { //RESULTADOS: Analiza el puntaje de cada jugador e imprime el resultado
@@ -95,12 +97,12 @@ public class Veintiuno { // Clase principal que dirige el juego dependiendo de l
      * @return retorna la carta eliminada del mazo o una carta de error en caso de no
      * encontrar una carta dentro del mazo
      */
-    public Carta sacarCartaMazo() {
+    private Carta sacarCartaMazo() {
         try {
             return mazo.remove((int) (Math.random() * mazo.size()));
         }catch (IndexOutOfBoundsException ex){
             System.out.println(ex.getMessage());
-            jugadorContinua = false;
+            juegoContinua = false;
         }
         return new Carta("-1","-1");
     }
@@ -112,7 +114,7 @@ public class Veintiuno { // Clase principal que dirige el juego dependiendo de l
      * @param nombreJugador string con el nombre del jugador
      * @return jugador encontrado o jugador creado y añadido al listado de jugadores
      */
-    public Jugador getJugador(String nombreJugador) {
+    private Jugador getJugador(String nombreJugador) {
         Optional<Jugador> optional = jugadores.stream().filter(
                 jugador -> nombreJugador.equals(jugador.getNombre()))
                 .findAny();
