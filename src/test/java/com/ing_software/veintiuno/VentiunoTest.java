17c1881f4ff521245b9bbfbc03da85c0f0e05651
Javier Melo <javier.hospital@bodytechcorp.com>
2021-09-27 22:09:27 -0500

    files for test

--- src/test/java/com/ing_software/veintiuno/VentiunoTest.java
+++ src/test/java/com/ing_software/veintiuno/VentiunoTest.java
@@ -1,2 +1,20 @@
-package com.ing_software.veintiuno;public class VentiunoTest {
+package com.ing_software.veintiuno;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VentiunoTest {
+
+    Veintiuno _ventiunoObject= new Veintiuno();
+
+    ///Solo es posible hacer prueba de nulidad ya que la lista de jugadores esta privada, por consiguiente no se cubrir
+    ///esa parte del código
+    @Test
+    public void getJugadorTest(){
+        assertEquals(null, _ventiunoObject.getJugador("casa"));
+        assertEquals(null, _ventiunoObject.getJugador("jugador"));
+        assertEquals(null, _ventiunoObject.getJugador(null));
+    }
+
 }
