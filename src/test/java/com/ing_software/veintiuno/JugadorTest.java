8c38ca67897021e360815e873b5314cd4b50b389
milo1998 <camiloerc_18@hotmail.com>
2021-10-08 16:49:55 -0500

    Update of unit test for class player

--- src/test/java/com/ing_software/veintiuno/JugadorTest.java
+++ src/test/java/com/ing_software/veintiuno/JugadorTest.java
@@ -18,30 +18,52 @@ public class JugadorTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		jugador = new Jugador("jugador");
-		jugador.addCarta(new Carta("3", "Corazones"));
+		//jugador.addCarta(new Carta("3", "Corazones"));
 		jugador.addCarta(new Carta("J", "Picas"));
 		jugador.addCarta(new Carta("As", "Picas"));
 	}
 
 	@Test
 	public void getCarta() {
+		// Debe retornar el array de cartas no vacío
 		assertNotNull(jugador.getCartas());
 	}
 
 	@Test
 	public void imprimirCartas() {
-		assertEquals("[(3, Corazones)], [(J, Picas)], [(As, Picas)]", jugador.imprimirCartas(false));
+		// Todas las cartas deben estar boca arriba
+		assertEquals("[(J, Picas)], [(As, Picas)]", jugador.imprimirCartas(false));
 	}
 
 	@Test
 	public void imprimirCartasOcultarPrimera() {
-		assertEquals("[(X, X)], [(J, Picas)], [(As, Picas)]", jugador.imprimirCartas(true));
+		// La primera carta debe estar boca abajo
+		assertEquals("[(X, X)], [(As, Picas)]", jugador.imprimirCartas(true));
+	}
+
+	@Test
+	public void getNombre() {
+		// Se debe retornar el nombre del jugador
+		assertEquals("jugador",jugador.getNombre());
 	}
 
 	@Test
 	public void getPuntaje() {
-		assertEquals("jugador",jugador.getNombre());
-		assertEquals(14,jugador.sumarPuntos());
-		assertEquals(14,jugador.getPuntaje());
+		//Debe retornar el puntaje del mazo del jugador
+		assertEquals(21,jugador.sumarPuntos());
+		assertEquals(21,jugador.getPuntaje());
+	}
+
+	@Test
+	public void testSumarPuntos() {
+		// Debe retornar la suma de puntos del mazo y tomar el As como 11
+		assertEquals(21, jugador.sumarPuntos());
+	}
+
+	@Test
+	public void testSumarPuntos2() {
+		jugador.addCarta(new Carta("3", "Corazones"));
+		//Debe retornar la suma de puntos del mazo y tomar el As como 1
+		assertEquals(14, jugador.sumarPuntos());
 	}
 }
